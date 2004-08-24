2492e2d4f9291cffa9ce58753e18646b2b10fd8c
m-arai <>
2004-08-24 11:14:19 +0000

    skip the reserved flag objects.
    reported by Anonymous contributor
    https://sourceforge.jp/forum/message.php?msg_id=10725

--- hos-v4/src/kernel/flg/ini_flg.c
+++ hos-v4/src/kernel/flg/ini_flg.c
@@ -2,7 +2,7 @@
 /*  Hyper Operating System V4  μITRON4.0仕様 Real-Time OS                  */
 /*    ITRONカーネル イベントフラグ                                          */
 /*                                                                          */
-/*                                  Copyright (C) 1998-2002 by Project HOS  */
+/*                                  Copyright (C) 1998-2004 by Project HOS  */
 /*                                  http://sourceforge.jp/projects/hos/     */
 /* ------------------------------------------------------------------------ */
 
@@ -19,11 +19,14 @@ void kernel_ini_flg(void)
 	/* イベントフラグの初期化 */
 	for ( i = KERNEL_TMIN_FLGID; i <= KERNEL_TMAX_FLGID; i++ )
 	{
-		KERNEL_FLGID_TO_FLGCB_RAM(i)->flgptn = KERNEL_FLGID_TO_FLGCB_RAM(i)->flgcb_rom->iflgptn;
+		if ( KERNEL_FLGID_TO_FLGCB_RAM(i) != NULL )
+		{
+			KERNEL_FLGID_TO_FLGCB_RAM(i)->flgptn = KERNEL_FLGID_TO_FLGCB_RAM(i)->flgcb_rom->iflgptn;
+		}
 	}
 }
 
 
 /* ------------------------------------------------------------------------ */
-/*  Copyright (C) 1998-2002 by Project HOS                                  */
+/*  Copyright (C) 1998-2004 by Project HOS                                  */
 /* ------------------------------------------------------------------------ */
